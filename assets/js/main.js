18e195a21926aad73d880994c4e543686296cee1
Blaser993 <corradodepinto.design@gmail.com>
2023-10-19 18:48:23 +0200

    add 404 error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,33 @@
-
-
-
 function searchCity() {
     var search = document.getElementById('search').value.trim().toLowerCase().replace(/\s+/g, '-');
     console.log(search)
+
     fetch(`https://api.teleport.org/api/urban_areas/slug:${search}/scores/`)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Città non trovata');
+            }
+            return response.json();
+        })
         .then(data => {
             console.log(data);
-
-            displayCityInfo(data)
+            displayCityInfo(data);
         })
         .catch(error => {
             console.error('Errore durante la chiamata delle API:', error);
-        })
+            displayErrorMessage('Città non trovata. Inserisci un nome di città valido.');
+        });
 }
-
 
 function displayCityInfo(data) {
-    // Recupera le informazioni desiderate dal risultato della chiamata API
-    var summary = data.summary;
-
-    // Visualizza le informazioni nell'area dedicata
-    var resultContainer = document.getElementById('resultContainer');
-    resultContainer.innerHTML =  summary
-                                
+    // Tua logica per visualizzare le informazioni della città
 }
+
+function displayErrorMessage(message) {
+    // Visualizza il messaggio di errore nell'HTML
+    var resultContainer = document.getElementById('resultError');
+    resultContainer.innerHTML = `<p style="color: red;">${message}</p>`;
+}
+
 
 
