e4e9a6a809544222377da24abc5b4e1e9e634cd9
Blaser993 <corradodepinto.design@gmail.com>
2023-10-19 17:17:07 +0200

    cercando di stampare a schermo le info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,29 @@
 
 
 
-function allCities(){
-    fetch(`https://api.teleport.org/api/cities/{?search}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(error =>{
-        console.error('Errore durante la chiamata delle API:', error);
-    })
+function searchCity() {
+    var search = document.getElementById('search').value.trim().toLowerCase().replace(/\s+/g, '-');
+    console.log(search)
+    fetch(`https://api.teleport.org/api/urban_areas/slug:${search}/scores/`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Errore durante la chiamata delle API:', error);
+        })
 }
 
 
-allCities();
+function displayCityInfo(data) {
+    // Recupera le informazioni desiderate dal risultato della chiamata API
+    var summary = data.summary;
+
+    // Visualizza le informazioni nell'area dedicata
+    var resultContainer = document.getElementById('resultContainer');
+    resultContainer.innerHTML = `<p>Nome della città: ${summary}</p>
+                                <p>Paese: ${country}</p>`;
+                                console.log(summary)
+}
 
 
-// function searchCity() {
-//     // Ottieni il valore dalla casella di testo
-//     var cityName = document.getElementById('cityInput').value;
-
-//     // Effettua una chiamata alle API di Teleport
-//     fetch(`https://api.teleport.org/api/urban_areas/?search=${cityName}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             // Visualizza le informazioni ottenute
-//             displayCityInfo(data);
-//             console.log(data)
-//         })
-//         .catch(error => {
-//             console.error('Errore durante la chiamata alle API:', error);
-//         });
-// }
-
-// function displayCityInfo(data) {
-//     // Recupera le informazioni desiderate dal risultato della chiamata API
-//     var cityName = data.geonames[0].name;
-//     var country = data.geonames[0].countryName;
-
-//     // Visualizza le informazioni nell'area dedicata
-//     var resultContainer = document.getElementById('resultContainer');
-//     resultContainer.innerHTML = `<p>Nome della città: ${cityName}</p>
-//                                 <p>Paese: ${country}</p>`;
-// }
