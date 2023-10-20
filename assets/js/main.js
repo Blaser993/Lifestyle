abaa9976303b5f12ca247dffb5abc97b448e1770
Blaser993 <corradodepinto.design@gmail.com>
2023-10-20 21:33:20 +0200

    score

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+//cerco la città
 function searchCity() {
     clear();
     let search = document.getElementById('search').value.trim().toLowerCase().replace(/\s+/g, '-');
@@ -13,6 +13,7 @@ function searchCity() {
         .then(data => {
             console.log(data);
             displayCityDescription(data)
+            displayCityScores(data)
         })
         .catch(error => {
             console.error('Errore durante la chiamata delle API:', error);
@@ -33,6 +34,8 @@ input.addEventListener("keypress", function(event) {
 function clear() {
     var resultDescription = document.getElementById('resultDescription');
     resultDescription.innerHTML = '';
+    var resultScores = document.getElementById('resultScores');
+    resultScores.innerHTML = '';
     var resultError = document.getElementById('resultError');
     resultError.innerHTML = '';
 }
@@ -43,13 +46,47 @@ function displayErrorMessage(message) {
     resultError.innerHTML = `<p style="color: red;">${message}</p>`;
 }
 
-// cerca la città in base al value nell'impit
+// mostra descrizione a schermo
 function displayCityDescription(data) {
     // Recupera le informazioni desiderate dal risultato della chiamata API
     const summary = data.summary;
     // Visualizza le informazioni nell'area dedicata
     let resultDescription = document.getElementById('resultDescription');
     resultDescription.innerHTML = summary                               
+}
+
+// mostro gli score a schermo
+function displayCityScores(data) {
+    const scoresArray = Object.entries(data.categories).map(([key, value]) => ({
+        category: key,
+        name: value.name,
+        score: value.score_out_of_10,
+        color: value.color
+    }));
+
+    scoresArray.forEach(score => {
+        let percentuale = (10*score.score).toFixed(2)
+
+        // Creo un nuovo contenitore
+        const article = document.createElement("div");
+        article.classList.add("col","card", "col-12" ,"col-sm-6","col-lg-4","col-xl-3" )
+        // Imposto il contenuto delle card
+        article.innerHTML = `
+        <div class="card-body m-1">
+            <p>${score.name}:<span> ${percentuale} di 10</span></p> 
+            <div class="bg-info-subtle" style="width: 100%;">
+                <div class="bg-info" style="width: ${percentuale}%; height: 20px;">
+                
+                </div>
+            </div>
+        </div>
+    `;
+
+        // Aggiungi il nuovo elemento di paragrafo a resultScores
+        resultScores.appendChild(article);
+
+        console.log("ecco i risultati:", score.name);
+    });
 }
 
 
